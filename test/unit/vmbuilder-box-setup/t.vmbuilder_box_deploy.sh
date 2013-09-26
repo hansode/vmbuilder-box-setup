f038b003ae176632b1e537e1a9210885df4fcff6
Masahito Yoshida <masahito@axsh.net>
2013-09-26 12:06:35 +0900

    "$? 0" -> "0 $?"

--- test/unit/vmbuilder-box-setup/t.vmbuilder_box_deploy.sh
+++ test/unit/vmbuilder-box-setup/t.vmbuilder_box_deploy.sh
@@ -22,12 +22,12 @@ function tearDown() {
 
 function test_vmbuilder_box_deploy_no_opts() {
   vmbuilder_box_deploy >/dev/null
-  assertNotEquals $? 0
+  assertNotEquals 0 $?
 }
 
 function test_vmbuilder_box_deploy_opts() {
   vmbuilder_box_deploy ${target_dir} >/dev/null
-  assertEquals $? 0
+  assertEquals 0 $?
 }
 
 ## shunit2
